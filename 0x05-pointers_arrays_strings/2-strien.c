1302c88d2d6941d1035f5611c834f5825227cb0d
JeresonAdj <akporjereson@gmail.com>
2022-10-24 13:34:18 -0700

    last commit

--- 0x05-pointers_arrays_strings/2-strien.c
+++ 0x05-pointers_arrays_strings/2-strien.c
@@ -2,16 +2,20 @@
 
 /**
  * _strlen - Returns the length of a string.
- * @str: string.
+ * @s: string.
  *
  * Return: Length.
  */
-size_t _strlen(const char *str)
+int  _strlen(char *s)
 {
-	size_t len = 0;
+	int len = 0;
 
-	while (*str++)
+	while (*s++ != '\0')
+	{
 		len++;
+		s++;
 
 	return (len);
+	}
+
 }
