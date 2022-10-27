e426dd9098c946bd8b6001a8204dfdb9d599093a
JeresonAdj <akporjereson@gmail.com>
2022-10-26 21:23:41 -0700

    second commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,21 @@
-#include "main.h"                                                                           
-                                                                                            
-/**                                                                                         
- * _strcat - concatennates two strings                                                      
- * @dest: copy to                                                                           
- * @src: copy from                                                                          
- * Return: pointer to dest                                                                  
- */                                                                                         
-char *_strcat(char *dest, char *src)                                                        
-{                                                                                           
-        int x;                                                                              
-        int y;                                                                              
-                                                                                            
-        x = 0;                                                                              
-        while (dest[x] != '\0')                                                             
-        {                                                                                   
-                x++;                                                                        
-        }                                                                                   
-        y = 0;                                                                              
-        while (src[y] != '\0')                                                              
-        {                                                                                   
-                dest[x] = src[y];                                                           
-                x++;                                                                        
-                y++;                                                                        
-        }                                                                                   
-        dest[x] = '\0';                                                                     
-        return (dest);                                                                      
-}     
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the string pointed to by @src,
+ * null byte, to the end of the string pointed to by @dest.
+ * @dest: A pointer to the string to be concatenated upon.
+ * @src: The source string to be appended to @dest.
+ *
+ * Return: A pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
